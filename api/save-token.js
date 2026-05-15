3545c1ee4bd10d5ab95121bce614457d700d1f09
claver35 <claver35@gmail.com>
2026-05-15 16:29:32 +0300

    Update save-token.js

--- api/save-token.js
+++ api/save-token.js
@@ -1,3 +1,4 @@
+const { encrypt } = require('./encrypt');
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
